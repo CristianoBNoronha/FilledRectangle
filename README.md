6f288496796d25bbd7699397804fd70dadb78a97
CristianoBNoronha <cris_nor@hotmail.com>
2022-08-09 11:40:30 -0300

    Add retangulo.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FilledRectangle
 Exercise following Python course specifications, to draw filled rectangle using # char.
+
+<a>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" width="60" height="60"/>
+<a/>
---
+++ retangulo.py
@@ -0,0 +1,28 @@
+
+l = int(input("digite a largura: "))
+h = int(input("digite a altura: "))
+rl = 1
+rh = 1
+while rh <= h:    
+    while rl <= l:            
+        print("#", end = "")
+        rl = rl + 1
+    rl = 1
+    rh = rh + 1
+    print()
+        
+    
+            
+
+            
+        
+            
+    
+    
+            
+
+
+
+    
+    
+        
